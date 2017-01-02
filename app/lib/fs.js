69776655f85cb888a5a79e9b52ff5e05c08276e5
kornalius <arianesoftinc@gmail.com>
2017-01-02 13:32:54 -0500

    fs, theme

--- app/lib/fs.js
+++ app/lib/fs.js
@@ -1,18 +1,97 @@
 import { Base } from '../objects/base.js'
 import { DB } from '../objects/db.js'
 import { path as _path } from '../utils.js'
+import { glob } from 'multimatch'
+import { Encoder, e, d } from './encoder.js'
 
 
 export const FS_DELIMITER = '/'
 
 
+export var errors = {
+  9: {
+    name: 'EBADF',
+    message: 'bad file descriptor',
+  },
+  10: {
+    name: 'EBUSY',
+    message: 'resource busy or locked',
+  },
+  18: {
+    name: 'EINVAL',
+    message: 'invalid argument',
+  },
+  27: {
+    name: 'ENOTDIR',
+    message: 'not a directory',
+  },
+  28: {
+    name: 'EISDIR',
+    message: 'illegal operation on a directory',
+  },
+  34: {
+    name: 'ENOENT',
+    message: 'no such file or directory',
+  },
+  47: {
+    name: 'EEXIST',
+    message: 'file already exists',
+  },
+  50: {
+    name: 'EPERM',
+    message: 'operation not permitted',
+  },
+  51: {
+    name: 'ELOOP',
+    message: 'too many symbolic links encountered',
+  },
+  53: {
+    name: 'ENOTEMPTY',
+    message: 'directory not empty',
+  },
+  55: {
+    name: 'EIO',
+    message: 'i/o error',
+  },
+}
+
+
+for (let k in errors) {
+  let errno = k
+  let errName = errors[k].name
+  let defaultMessage = errors[k].message
+
+  let FSError = class FSError extends Error {
+    constructor (path, msg) {
+      super()
+      this.name = errName
+      this.code = errName
+      this.errno = errno
+      this.message = msg || defaultMessage
+      this.path = path
+      this.stack = (new Error(this.message)).stack
+    }
+
+    toString () {
+      var pathInfo = this.path ? ', \'' + this.path + '\'' : ''
+      return this.name + ': ' + this.message + pathInfo
+    }
+  }
+
+  errors[errno] = FSError
+  errors[errName] = FSError
+}
+
+
 export class FileEntry extends Base {
 
-  constructor (options = {}) {
+  constructor (fs, parent, options = {}) {
     super(options)
 
-    this._parent = _.get(options, 'parent')
+    this._fs = fs
+    this._parent = parent
     this._files = []
+
     this._name = _.get(options, 'name')
     this._attr = _.get(options, 'attr', 'rwx')
     this._created = _.get(options, 'created', 0)
@@ -20,7 +99,11 @@ export class FileEntry extends Base {
     this._size = _.get(options, 'size', 0)
   }
 
+  get fs () { return this._fs }
   get parent () { return this._parent }
+
+  get id () { return this.path.replace(FS_DELIMITER, '$') }
+
   get name () { return this._name }
   get attr () { return this._attr }
   get created () { return this._created }
@@ -28,15 +111,17 @@ export class FileEntry extends Base {
   get size () { return this._size }
 
   get files () { return this._files }
+  get folders () { _.map(this._files, f => f.folder) }
 
   get readable () { return this._attr.indexOf('r') !== -1 }
   get writable () { return this._attr.indexOf('w') !== -1 }
   get executable () { return this._attr.indexOf('x') !== -1 }
   get folder () { return this._attr.indexOf('d') !== -1 }
+  get file () { return !this.folder }
 
   get isRoot () { return !this._parent }
 
-  find (name) { return _.find(this.files, { name }) }
+  get extname () { return _path.extname(this._name) }
 
   get root () {
     let p = this
@@ -56,40 +141,318 @@ export class FileEntry extends Base {
     return paths.join(FS_DELIMITER)
   }
 
-  get basename () { return _path.basename(this._name) }
-  get ext () { return _path.extname(this._name) }
+  find (name) {
+    return new Promise((resolve, reject) => {
+      let f = _.find(this.files, { name })
+      return f ? resolve(f) : reject(new errors.ENOENT(this.path))
+    })
+  }
 
-  read () { return DB.get(this.name) }
+  create (name, data, attr = 'rwx') {
+    return new Promise((resolve, reject) => {
+      if (this.exists(name)) {
+        return reject(new errors.EEXIST(this.path))
+      }
+      let f = new FileEntry(this.fs, this, { name, attr })
+      this._files.push(f)
+      if (data) {
+        return f.write(data).then(f.touch).then(resolve)
+      }
+      return f.touch().then(resolve)
+    })
+  }
 
-  write (data) { return DB.put(this.name, data) }
+  mkdir (name, attr = 'rw') {
+    return this.create(name, null, attr + 'd')
+  }
+
+  touch () {
+    this._modified = Date.now()
+    if (!this._created) {
+      this._created = this._modified
+    }
+    return this.fs.save()
+  }
+
+  rm () {
+    return new Promise((resolve, reject) => {
+      if (this.file) {
+        return DB.remove(this.id).then(() => {
+          _.remove(this.parent.files, f => f.name === this.name)
+          return this.fs.save().then(resolve)
+        })
+      }
+      else if (this.folder) {
+        if (this.files.length) {
+          return reject(new errors.ENOTEMPTY(this.path))
+        }
+        else {
+          _.remove(this.parent.files, f => f.name === this.name)
+        }
+        return this.fs.save().then(resolve)
+      }
+      else {
+        return reject(new errors.EPERM(this.path))
+      }
+    })
+  }
+
+  read () {
+    return DB.get(this.id)
+  }
+
+  write (data) {
+    return DB.put(this.id, data).then(() => {
+      this._size = data.length
+      return this.touch()
+    })
+  }
+
+  match (patterns = ['*']) { return glob([this._name], patterns) }
+
+  ls (patterns = ['*'], deep = false) {
+    return new Promise((resolve, reject) => {
+      let files = []
+      for (let f of this.files) {
+        if (f.match(patterns)) {
+          files.push(f)
+        }
+        if (deep && f.folder) {
+          files.concat(f.ls(patterns, true))
+        }
+      }
+      return resolve(files)
+    })
+  }
+
+  rename (name) {
+    return this.exists(name).catch(() => {
+      this._name = name
+      return this.touch()
+    })
+  }
+
+  copy (dest) {
+
+  }
+
+  move (dest) {
+
+  }
 
 }
 
 
 export class FS extends Base {
 
+  static paths (path) {
+    path = FS.normalize(path)
+    return path.split(FS_DELIMITER)
+  }
+
+  static realPath (path) {
+    return _path.sep !== FS_DELIMITER ? path.replace(FS_DELIMITER, _path.sep) : path
+  }
+
+  static fakePath (path) {
+    return _path.sep !== FS_DELIMITER ? path.replace(_path.sep, FS_DELIMITER) : path
+  }
+
+  static normalize (path) {
+    return FS.fakePath(FS.normalize(FS.realPath(path)))
+  }
+
+  static parse (path) { return _path.parse(FS.realPath(path)) }
+
+  static resolve () { return _path.resolve(...arguments) }
+
+  static join () { return _path.join(...arguments) }
+
+  static isAbsolute (path) { return _path.isAbsolute(FS.realPath(path)) }
+
+  static dirname (path) { return FS.fakePath(_path.dirname(FS.realPath(path))) }
+
+  static basename (path) { return _path.basename(FS.realPath(path)) }
+
+  static extname (path) { return _path.extname(FS.realPath(path)) }
+
   constructor (options = {}) {
     super(options)
 
     this._root = _.get(options, 'root', new FileEntry({ name: FS_DELIMITER, attr: 'rd' }))
-    this._pwd = _.get(options, 'pwd', this._root)
   }
 
   get root () { return this._root }
-  get pwd () { return this._pwd }
 
-  find (path) {
-    path = _path.normalize(path)
-    let fe = null
-    for (let p of path.split(FS_DELIMITER)) {
-      fe = (fe || this.root).find(p)
-      if (!fe) {
-        break
-      }
-    }
-    return fe
+  load (name = '_catalog') {
+    return DB.get(name).then(Encoder.decode)
   }
 
-  exists (path) { return this.find(path) !== null }
+  save (name = '_catalog') {
+    return DB.put(name, Encoder.encode(this._root))
+  }
+
+  find (path) {
+    return new Promise((resolve, reject) => {
+      let fe = null
+      for (let p of this.paths(path)) {
+        fe = (fe || this.root).find(p)
+        if (!fe) {
+          break
+        }
+      }
+      return fe ? resolve(fe) : reject(new errors.ENOENT(path))
+    })
+  }
+
+  ensurePath (path, attr = 'rwx') {
+    return new Promise((resolve, reject) => {
+      let paths = this.paths(path)
+      let f = this.root
+      let len = paths.length
+      for (let i = 0; i < len; i++) {
+        let p = paths[i]
+        let nf = f.find(p)
+        if (!nf) {
+          if (i === len - 1) {
+            nf = f.create(p, null, attr)
+          }
+          else {
+            nf = f.mkdir(p)
+          }
+        }
+        f = nf
+      }
+      return resolve(f)
+    })
+  }
+
+  create (path, data, attr) {
+    return this.find(path).catch(() => {
+      return this.find(FS.dirname(path)).then(d => d.create(FS.basename(path), data, attr))
+    })
+  }
+
+  mkdir (path, attr) {
+    return this.create(path, null, (attr || '') + 'd')
+  }
+
+  rm (path) {
+    return this.find(path).then(f => f.rm())
+  }
+
+  ls (path, glob) {
+    return this.find(path).then(f => {
+      return f.folder ? f.files : Promise.reject(new errors.ENOTDIR(path))
+    })
+  }
+
+  read (path) {
+    return this.find(path).then(f => f.read())
+  }
+
+  write (path, data) {
+    return this.find(path).then(f => f.write(data))
+  }
+
+  attr (path, value) {
+    return this.find(path).then(f => {
+      if (value) {
+        f.attr = value
+        this.save()
+      }
+      return f.attr
+    })
+  }
+
+  size (path) {
+    return this.find(path).then(f => f.size)
+  }
+
+  isFile (path) {
+    return this.find(path).then(f => f.file)
+  }
+
+  isFolder (path) {
+    return this.find(path).then(f => f.folder)
+  }
+
+  isReadable (path) {
+    return this.find(path).then(f => f.readable)
+  }
+
+  isWritable (path) {
+    return this.find(path).then(f => f.writable)
+  }
+
+  isExecutable (path) {
+    return this.find(path).then(f => f.executable)
+  }
+
+  created (path) {
+    return this.find(path).then(f => f.created)
+  }
+
+  modified (path) {
+    return this.find(path).then(f => f.modified)
+  }
+
+  copy (path, newpath) {
+    return this.find(path).then(f => {
+    })
+  }
+
+  move (path, newpath) {
+    return this.find(path).then(f => {
+    })
+  }
 
 }
+
+
+export class Shell extends Base {
+
+  constructor (options = {}) {
+    super(options)
+    this._cwd = _.get(options, 'cwd', this._root)
+  }
+
+  get cwd () { return this._cwd }
+
+  cd (path) {
+
+  }
+
+}
+
+
+Encoder.register('FileEntry', {
+
+  encode: obj => {
+    let doc = {}
+    e('type', obj, doc)
+    e('name', obj, doc)
+    e('attr', obj, doc)
+    e('created', obj, doc)
+    e('modified', obj, doc)
+    e('size', obj, doc)
+    for (let i = 0; i < obj.files.length; i++) {
+      doc.files[i] = Encoder.encode(obj.files[i])
+    }
+    return doc
+  },
+
+  decode: (doc, obj) => {
+    obj = obj || new FileEntry()
+    d('type', doc, obj)
+    d('name', doc, obj)
+    d('attr', doc, obj)
+    d('created', doc, obj)
+    d('modified', doc, obj)
+    d('size', doc, obj)
+    for (let i = 0; i < doc.files.length; i++) {
+      obj.files[i] = Encoder.decode(doc.files[i])
+    }
+    return obj
+  },
+})
---
+++ app/lib/theme.js
@@ -0,0 +1,11 @@
+import { Base } from '../objects/base.js'
+import { DB } from '../objects/db.js'
+
+
+export class Theme extends Base {
+
+  constructor (options = {}) {
+    super(options)
+  }
+
+}
